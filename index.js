a412835bc7fbc3ca597743a0ccf549cd5a56fa11
rvlbot <74479717+rvlbot@users.noreply.github.com>
2021-01-01 15:11:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ client.on('group-participants-update', async (anu) => {
 				success: '️❬ ✔ ❭ 𝘀𝘂𝗰𝗰𝗲𝘀𝘀 𝘁𝗼𝗱🖤',
 				error: {
 					stick: '*[GAGAL]* *Coba Lagi Yah...*'
-					Iv: 'Link Tidak Valid🙂'
+					Iv:'Link Tidak Valid🙂',
 				},
 				only: {
 					group: '❬❗❭ 𝗚𝗥𝗢𝗨𝗣 𝗢𝗡𝗟𝗬',
